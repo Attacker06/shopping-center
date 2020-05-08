63c16a46da9fe17f6d96c6b91b5b8eab334f3492
kai <a0936526788@gmail.com>
2020-05-08 15:03:14 +0800

    1112

--- src/main/java/com/kai/shoppingmall/entity/ShoppingList.java
+++ src/main/java/com/kai/shoppingmall/entity/ShoppingList.java
@@ -1,4 +1,52 @@
 package com.kai.shoppingmall.entity;
 
+import javax.persistence.*;
+
+@Table
+@Entity
 public class ShoppingList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer shoppingListId;
+
+    @Column
+    private Integer quantity;
+
+    @JoinColumn
+    @ManyToOne
+    private Product product;
+
+    @JoinColumn
+    @ManyToOne
+    private Member member;
+
+    public Integer getShoppingListId() {
+        return shoppingListId;
+    }
+    public void setShoppingListId(Integer shoppingListId) {
+        this.shoppingListId = shoppingListId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+    public void setMember(Member member) {
+        this.member = member;
+    }
+}
 }
