4265e5c81ddc1a7df51b2ac53b127b7f7794c8c2
kai <a0936526788@gmail.com>
2020-05-08 15:00:19 +0800

    1112

--- src/main/java/com/kai/shoppingmall/entity/Employee.java
+++ src/main/java/com/kai/shoppingmall/entity/Employee.java
@@ -1,4 +1,51 @@
 package com.kai.shoppingmall.entity;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer employeeId;
+
+    @Column
+    private String employeeName;
+
+    @Column
+    private String employeeGender;
+
+    @JoinColumn
+    @OneToMany
+    private List<ProblemReport> problemReportList;
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+    public void setEmployeeId(Integer employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmployeeGender() {
+        return employeeGender;
+    }
+    public void setEmployeeGender(String employeeGender) {
+        this.employeeGender = employeeGender;
+    }
+
+    public List<ProblemReport> getProblemReportList() {
+        return problemReportList;
+    }
+    public void setProblemReportList(List<ProblemReport> problemReportList) {
+        this.problemReportList = problemReportList;
+    }
 }
