c03bb86e102c882540648422f1e2f437a4025b58
kai <a0936526788@gmail.com>
2020-05-12 17:09:48 +0800

    0512

--- src/main/java/com/kai/shoppingmall/service/impl/MemberService.java
+++ src/main/java/com/kai/shoppingmall/service/impl/MemberService.java
@@ -1,8 +1,84 @@
 package com.kai.shoppingmall.service.impl;
 
+import com.kai.shoppingmall.entity.Member;
+import com.kai.shoppingmall.repository.MemberRepository;
+import com.kai.shoppingmall.response.BaseResponse;
 import com.kai.shoppingmall.service.IMemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService implements IMemberService {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Override
+    public BaseResponse findAll() {
+        BaseResponse baseResponse = new BaseResponse();
+        List<Member> memberList = memberRepository.findAll();
+        baseResponse.setStatus(true);
+        baseResponse.setMessage("查詢成功");
+        baseResponse.setData(memberList);
+        return baseResponse;
+    }
+
+    @Override
+    public BaseResponse select(Integer id) {
+        BaseResponse baseResponse = new BaseResponse();
+        try {
+            Member member = memberRepository.getOne(id);
+            baseResponse.setStatus(true);
+            baseResponse.setMessage("查詢成功");
+            baseResponse.setData(member);
+        }catch (Exception e){
+            baseResponse.setStatus(false);
+            baseResponse.setMessage("查詢失敗");
+        }
+        return baseResponse;
+    }
+
+    @Override
+    public BaseResponse insert(Member member) {
+        BaseResponse baseResponse = new BaseResponse();
+        try {
+            memberRepository.save(member);
+            baseResponse.setStatus(true);
+            baseResponse.setMessage("新增成功");
+        }catch (Exception e){
+            baseResponse.setStatus(false);
+            baseResponse.setMessage("新增失敗");
+        }
+        return baseResponse;
+    }
+
+    @Override
+    public BaseResponse update(Member member) {
+        BaseResponse baseResponse = new BaseResponse();
+        try {
+            memberRepository.save(member);
+            baseResponse.setStatus(true);
+            baseResponse.setMessage("修改成功");
+        }catch (Exception e){
+            baseResponse.setStatus(false);
+            baseResponse.setMessage("修改失敗");
+        }
+        return baseResponse;
+    }
+
+    @Override
+    public BaseResponse delete(Integer id) {
+        BaseResponse baseResponse = new BaseResponse();
+        try {
+            memberRepository.deleteById(id);
+            baseResponse.setStatus(true);
+            baseResponse.setMessage("刪除成功");
+        }catch (Exception e){
+            baseResponse.setStatus(false);
+            baseResponse.setMessage("刪除失敗");
+        }
+        return baseResponse;
+    }
 }
