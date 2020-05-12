3199b4f92b6a8a5809520e7f82e5357f07621553
kai <a0936526788@gmail.com>
2020-05-12 17:09:12 +0800

    0512

--- src/main/java/com/kai/shoppingmall/service/IMemberService.java
+++ src/main/java/com/kai/shoppingmall/service/IMemberService.java
@@ -1,4 +1,16 @@
 package com.kai.shoppingmall.service;
 
+import com.kai.shoppingmall.entity.Member;
+import com.kai.shoppingmall.response.BaseResponse;
+
 public interface IMemberService {
+    BaseResponse findAll();
+
+    BaseResponse select(Integer id);
+
+    BaseResponse insert(Member member);
+
+    BaseResponse update(Member member);
+
+    BaseResponse delete(Integer id);
 }
